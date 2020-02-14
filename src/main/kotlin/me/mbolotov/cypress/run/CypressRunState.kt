00cc7803f8b19528bcde667d6c34c506e879d166
Mikhail Bolotov <mikhail.bolotov@gmail.com>
2020-02-14 23:08:16 +0300

    moving under common js test framework

--- src/main/kotlin/me/mbolotov/cypress/run/CypressRunState.kt
+++ src/main/kotlin/me/mbolotov/cypress/run/CypressRunState.kt
@@ -1,51 +1,93 @@
 package me.mbolotov.cypress.run
 
-import com.intellij.execution.configurations.CommandLineState
-import com.intellij.execution.process.*
+import com.intellij.execution.DefaultExecutionResult
+import com.intellij.execution.ExecutionException
+import com.intellij.execution.ExecutionResult
+import com.intellij.execution.configuration.EnvironmentVariablesData
+import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.execution.process.ProcessTerminatedListener
 import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.execution.testframework.sm.SMTestRunnerConnectionUtil
+import com.intellij.execution.ui.ConsoleView
+import com.intellij.javascript.debugger.CommandLineDebugConfigurator
+import com.intellij.javascript.nodejs.NodeCommandLineUtil
+import com.intellij.javascript.nodejs.NodeConsoleAdditionalFilter
+import com.intellij.javascript.nodejs.NodeStackTraceFilter
+import com.intellij.javascript.nodejs.debug.NodeLocalDebuggableRunProfileStateSync
+import com.intellij.javascript.nodejs.interpreter.NodeCommandLineConfigurator
+import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreter
 import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreterRef
-import com.intellij.openapi.diagnostic.logger
-import com.intellij.openapi.fileEditor.FileDocumentManager
-import com.intellij.openapi.util.io.FileUtilRt
-import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.util.SystemInfo
+import com.intellij.openapi.util.io.FileUtil
+import com.jetbrains.nodejs.util.NodeJsCodeLocator
 import java.io.File
+import java.io.IOException
+import java.nio.charset.StandardCharsets
 
-class CypressRunState(environment: ExecutionEnvironment?, private val configuration: CypressRunConfig) : CommandLineState(environment) {
-    override fun startProcess(): ProcessHandler {
-        var specFile = configuration.getPersistentData().specFile
-        val textRange = configuration.getPersistentData().textRange
-        var onlyFile: File? = null
-        if (textRange != null && specFile != null) {
-            val virtualFile = LocalFileSystem.getInstance().findFileByIoFile(File(configuration.getPersistentData().specFile!!))
-            val doc = FileDocumentManager.getInstance().getDocument(virtualFile!!)
-            if (doc != null) {
-                val text = doc.text
-                val case = text.substring(textRange.startOffset, textRange.endOffset).replaceFirst(testKeyword, "${testKeyword}.only")
-                val only = text.substring(0, textRange.startOffset) + case + text.substring(textRange.endOffset)
-                val orig = File(specFile)
-                try {
-                    val ext = FileUtilRt.getExtension(specFile)
-                    onlyFile = File(orig.parent, "__only." + ext)
-                    onlyFile.writeBytes(only.toByteArray())
-                    specFile = onlyFile.absolutePath
-                } catch (e: Exception) {
-                    logger<CypressRunState>().error("failed to write the 'only' spec", e)
-                }
+class CypressRunState(private val myEnv: ExecutionEnvironment, private val myRunConfiguration: CypressRunConfig) : NodeLocalDebuggableRunProfileStateSync() {
+    private val myProject = myEnv.project
+
+    override fun executeSync(configurator: CommandLineDebugConfigurator?): ExecutionResult {
+        val interpreter: NodeJsInterpreter = NodeJsInterpreterRef.create(this.myRunConfiguration.getPersistentData().nodeJsRef).resolveNotNull(myEnv.project)
+        val commandLine = NodeCommandLineUtil.createCommandLine(if (SystemInfo.isWindows) false else null)
+        NodeCommandLineUtil.configureCommandLine(commandLine, configurator) { debugMode: Boolean -> this.configureCommandLine(commandLine, interpreter, debugMode) }
+        val processHandler = NodeCommandLineUtil.createProcessHandler(commandLine, false)
+        val consoleProperties = CypressConsoleProperties(this.myRunConfiguration, this.myEnv.getExecutor(), CypressTestLocationProvider(), NodeCommandLineUtil.shouldUseTerminalConsole(processHandler))
+        val consoleView: ConsoleView = this.createSMTRunnerConsoleView(commandLine.workDirectory, consoleProperties)
+        ProcessTerminatedListener.attach(processHandler)
+        consoleView.attachToProcess(processHandler)
+        val executionResult = DefaultExecutionResult(consoleView, processHandler)
+//        executionResult.setRestartActions(consoleProperties.createRerunFailedTestsAction(consoleView))
+        return executionResult
+    }
+
+    private fun createSMTRunnerConsoleView(workingDirectory: File?, consoleProperties: CypressConsoleProperties): ConsoleView {
+        val consoleView: ConsoleView = SMTestRunnerConnectionUtil.createConsole(consoleProperties.testFrameworkName, consoleProperties)
+        consoleProperties.addStackTraceFilter(NodeStackTraceFilter(this.myProject, workingDirectory))
+        consoleProperties.stackTrackFilters.forEach { consoleView.addMessageFilter(it) }
+        consoleView.addMessageFilter(NodeConsoleAdditionalFilter(this.myProject, workingDirectory))
+        Disposer.register(this.myProject, consoleView)
+        return consoleView
+    }
+
+    private fun configureCommandLine(commandLine: GeneralCommandLine, interpreter: NodeJsInterpreter, debugMode: Boolean) {
+        commandLine.charset = StandardCharsets.UTF_8
+        val data = this.myRunConfiguration.getPersistentData()
+        val workingDirectory = data.getWorkingDirectory()
+        if (workingDirectory.isNotBlank()) {
+            commandLine.withWorkDirectory(workingDirectory)
+        }
+        NodeCommandLineUtil.configureUsefulEnvironment(commandLine)
+        NodeCommandLineUtil.prependNodeDirToPATH(commandLine, interpreter)
+        commandLine.withParameters("node_modules/cypress/bin/cypress", "run")
+        if (data.additionalParams.isNotBlank()) {
+            commandLine.withParameters(data.additionalParams.split("\\s+".toRegex()))
+        }
+        EnvironmentVariablesData.create(data.envs, data.passParentEnvs).configureCommandLine(commandLine, true)
+        if (debugMode) {
+        }
+        commandLine.addParameter("--reporter")
+        commandLine.addParameter(getMochaReporterFile().absolutePath)
+        when (data.kind) {
+            CypressRunConfig.TestKind.DIRECTORY -> {
+                // todo integration folder
+                commandLine.addParameter(FileUtil.toSystemDependentName(data.specsDir!!))
             }
+            CypressRunConfig.TestKind.SPEC -> {
+                commandLine.withParameters("-s", data.specFile)
+            }
+            CypressRunConfig.TestKind.TEST -> TODO()
+            CypressRunConfig.TestKind.SUITE -> TODO()
         }
+        NodeCommandLineConfigurator.find(interpreter).configure(commandLine)
+    }
 
-        val interperter = NodeJsInterpreterRef.create(configuration.getPersistentData().nodeJsRef).resolveAsLocal(environment.project)
-        val cmd = "${interperter.interpreterSystemIndependentPath} node_modules/cypress/bin/cypress run ${configuration.getPersistentData().additionalParams} -s ${specFile}"
-        val handler = KillableProcessHandler(Runtime.getRuntime().exec(cmd, null, File(configuration.getWorkingDirectory()
-                ?: ".")), cmd)
-        if (textRange != null && specFile != null) {
-            handler.addProcessListener(object : ProcessAdapter() {
-                override fun processTerminated(event: ProcessEvent) {
-                    onlyFile?.delete()
-                }
-            })
+    private fun getMochaReporterFile(): File {
+        return try {
+            NodeJsCodeLocator.getFileRelativeToJsDir("mocha-intellij/lib/mochaIntellijReporter.js")
+        } catch (e: IOException) {
+            throw ExecutionException("IntelliJ Mocha reporter not found", e)
         }
-        ProcessTerminatedListener.attach(handler)
-        return handler
     }
 }
